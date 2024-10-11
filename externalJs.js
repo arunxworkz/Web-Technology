2d26040d6a03431c3382b86fcbbf907aaadeb002
belavadi123 <arunbelavadi40@gmail.com>
2024-10-11 12:34:21 +0530

    Calculator

--- externalJs.js
+++ externalJs.js
@@ -1,1 +1,10 @@
 console.log("This is java Script")
+
+function calculation(){
+    const firstNumber = parseInt(document.getElementById('firstNumber').value);
+    const secondNumber = parseInt(document.getElementById('secondNumber').value);
+
+    const result = firstNumber + secondNumber;
+
+    document.getElementById('result').innerText = result;
+}
